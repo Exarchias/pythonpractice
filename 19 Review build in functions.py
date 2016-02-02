2a09656885033db0ff9f2acc1a5d4e7e7c4fb3f7
Robert Kristian Alm <exarchiasghost@gmail.com>
2016-02-02 16:43:52 +0100

    19 Review. Build-In functions.
    Instructions
    First, def a function called distance_from_zero, with one argument (choose any argument name you like).
    If the type of the argument is either int or float, the function should return the absolute value of the function input.
    Otherwise, the function should return "Nope"

---
+++ 19 Review build in functions.py
@@ -0,0 +1,9 @@
+def distance_from_zero(pen):
+    if type(pen) == int or type(pen) == float:
+        return abs(pen)
+        
+    else:
+        return "Nope"
+        
+        
+distance_from_zero(-10)
